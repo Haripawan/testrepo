87bbb69f1f81e7dde15ee6bb9e8b0c4e13583c83
Haripawan <48235958+Haripawan@users.noreply.github.com>
2024-05-01 11:51:10 +0100

    Update sqlglot_linegae.sql

--- sqlglot_linegae.sql
+++ sqlglot_linegae.sql
@@ -123,3 +123,47 @@ for line in lineage:
     print(f"Column: {line['column']}")
     print(f"Lineage: {', '.join(line['lineage'])}")
 
+#######version 4########
+
+
+To extract the lineage information from the Oracle SQL query and output it as JSON, you can modify the Python code to generate a JSON object containing the tables and columns involved. Here's how you can do it:
+
+```python
+import json
+from sqlglot import parse
+
+def extract_tables_and_columns(node):
+    tables = []
+    columns = []
+    if node.type == 'Identifier':
+        if node.is_child_of('From'):
+            tables.append(node.get_source())
+        elif node.is_child_of('Select'):
+            columns.append(node.value)
+    for child in node.children:
+        child_tables, child_columns = extract_tables_and_columns(child)
+        tables.extend(child_tables)
+        columns.extend(child_columns)
+    return tables, columns
+
+def trace_data_lineage(sql_query):
+    parsed_query = parse(sql_query, flavor='oracle')
+    tables, columns = extract_tables_and_columns(parsed_query)
+    return {"tables": tables, "columns": columns}
+
+# Example Oracle SQL query with dynamic joins and inner queries
+oracle_query = """
+SELECT t1.col1, t2.col2
+FROM table1 t1
+JOIN table2 t2 ON t1.id = t2.id
+WHERE t1.col3 IN (SELECT col FROM table3 WHERE condition)
+"""
+
+# Trace data lineage
+lineage_json = trace_data_lineage(oracle_query)
+
+# Output lineage as JSON
+print(json.dumps(lineage_json, indent=4))
+```
+
+This code will output the lineage information extracted from the Oracle SQL query as a JSON object. The JSON object will have two keys: "tables" containing a list of tables involved and "columns" containing a list of columns selected in the query. You can further extend this code to include additional information or structure the JSON output according to your requirements.
