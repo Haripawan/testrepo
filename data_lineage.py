41f92fb7c5cce32848692a46129bdf1ced0fd503
Haripawan <48235958+Haripawan@users.noreply.github.com>
2024-07-04 16:55:52 +0100

    Update data_lineage.py

--- data_lineage.py
+++ data_lineage.py
@@ -1,4 +1,6 @@
 import sqlparse
+from sqlparse.sql import IdentifierList, Identifier
+from sqlparse.tokens import Keyword, DML
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -10,12 +12,50 @@ def read_sql_file(file_path):
 def parse_sql(sql_script):
     return sqlparse.parse(sql_script)
 
+def is_subselect(parsed):
+    if not parsed.is_group():
+        return False
+    for item in parsed.tokens:
+        if item.ttype is DML and item.value.upper() == 'SELECT':
+            return True
+    return False
+
+def extract_from_part(parsed):
+    from_seen = False
+    for item in parsed.tokens:
+        if from_seen:
+            if is_subselect(item):
+                for x in extract_from_part(item):
+                    yield x
+            elif item.ttype is Keyword:
+                return
+            elif item.ttype is None:
+                yield item
+        elif item.ttype is Keyword and item.value.upper() == 'FROM':
+            from_seen = True
+
+def extract_table_identifiers(token_stream):
+    identifiers = []
+    for item in token_stream:
+        if isinstance(item, IdentifierList):
+            for identifier in item.get_identifiers():
+                identifiers.append(identifier.get_real_name())
+        elif isinstance(item, Identifier):
+            identifiers.append(item.get_real_name())
+    return identifiers
+
 def extract_table_column_references(parsed_sql):
     table_references = {}
     for statement in parsed_sql:
         if statement.get_type() == 'SELECT':
-            tables = [token.get_real_name() for token in statement.tokens if token.ttype is None and token.get_real_name()]
-            columns = [token.get_real_name() for token in statement.tokens if token.ttype is not None and token.get_real_name()]
+            tables = extract_table_identifiers(extract_from_part(statement))
+            columns = []
+            for token in statement.tokens:
+                if isinstance(token, IdentifierList):
+                    for identifier in token.get_identifiers():
+                        columns.append(identifier.get_real_name())
+                elif isinstance(token, Identifier):
+                    columns.append(token.get_real_name())
             for table in tables:
                 if table not in table_references:
                     table_references[table] = set()
